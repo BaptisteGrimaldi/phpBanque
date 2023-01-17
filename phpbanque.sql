67b709c758b6de2aeed0a7e6f053f5a54ed96b5c
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-17 11:19:52 +0100

    réglage bdd

--- phpbanque.sql
+++ phpbanque.sql
@@ -39,13 +39,15 @@ CREATE TABLE `bankaccounts` (
 -- Table structure for table `contact_forms`
 --
 
+DROP TABLE IF EXISTS `contact_forms`;
 CREATE TABLE `contact_forms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fullname` varchar(100) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `message` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -84,13 +86,15 @@ CREATE TABLE `transactions` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL	AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_ip` varchar(80) NOT NULL
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `last_ip` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
