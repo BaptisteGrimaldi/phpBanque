1e4ff8bc7b69bcadaa9a7509c0abba67decd9f85
OG <103863031+RyzKum@users.noreply.github.com>
2023-01-18 11:08:59 +0100

    sql

--- phpbanque.sql
+++ phpbanque.sql
@@ -1,14 +1,12 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 17, 2023 at 09:49 AM
--- Server version: 5.7.24
--- PHP Version: 8.1.0
+-- Hôte : localhost:8889
+-- Généré le : mer. 18 jan. 2023 à 10:06
+-- Version du serveur :  5.7.34
+-- Version de PHP : 8.0.8
 
-SET NAMES utf8;
-SET foreign_key_checks = 0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -20,92 +18,185 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `phpbanque`
+-- Base de données : `phpbanque`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bankaccounts`
+-- Structure de la table `contact_forms`
 --
 
-CREATE TABLE `bankaccounts` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contact_forms`
---
-
-DROP TABLE IF EXISTS `contact_forms`;
 CREATE TABLE `contact_forms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fullname` varchar(100) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `message` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `monnaie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `contact_forms`
+--
+
+INSERT INTO `contact_forms` (`id`, `fullname`, `phone`, `created_at`, `monnaie`) VALUES
+(1, 'New fullname', '09090909', '2023-01-16 10:50:29', ''),
+(2, 'Test 2', '08080808', '2023-01-16 11:03:23', ''),
+(81, 'Test fullname', '08020282924', '2023-01-17 09:54:54', ''),
+(82, 'Test fullname', '08020282924', '2023-01-17 10:06:25', ''),
+(83, 'Test fullname', '08020282924', '2023-01-17 10:09:52', ''),
+(84, 'Test fullname', '08020282924', '2023-01-17 10:12:39', ''),
+(85, 'Test fullname', '08020282924', '2023-01-17 10:18:56', ''),
+(86, 'Test fullname', '08020282924', '2023-01-17 10:18:58', ''),
+(87, 'Test fullname', '08020282924', '2023-01-17 10:36:25', ''),
+(88, 'Test fullname', '08020282924', '2023-01-17 10:45:39', ''),
+(89, 'Test fullname', '08020282924', '2023-01-17 10:45:54', ''),
+(90, 'Test fullname', '08020282924', '2023-01-17 10:47:44', ''),
+(91, 'Test fullname', '08020282924', '2023-01-17 11:13:53', ''),
+(92, 'Test fullname', '08020282924', '2023-01-17 11:26:59', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `currencies`
+-- Structure de la table `currencies`
 --
 
 CREATE TABLE `currencies` (
-  `id` int(11) NOT NULL
+  `id_monnaie` int(11) NOT NULL,
+  `nom_monnaie` int(11) NOT NULL,
+  `taux_change` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `deposits`
+-- Structure de la table `deposits`
 --
 
 CREATE TABLE `deposits` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  `date_depos` datetime NOT NULL,
+  `monnaie` int(11) NOT NULL,
+  `description_depot` varchar(255) NOT NULL,
+  `id_depo` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Structure de la table `transactions`
 --
 
 CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `montant_depot` int(100) NOT NULL,
+  `date_depos` datetime NOT NULL,
+  `date_retrait` datetime NOT NULL,
+  `description_depot` varchar(255) NOT NULL,
+  `montant_retrait` int(100) NOT NULL,
+  `id_transaction` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `last_ip` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_transaction` int(11) NOT NULL,
+  `bankaccounts` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `withdrawals`
+-- Structure de la table `withdrawals`
 --
 
 CREATE TABLE `withdrawals` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  `monnaie` varchar(200) NOT NULL,
+  `date_retrait` datetime NOT NULL,
+  `montant_retrait` int(11) NOT NULL,
+  `id_retrait` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `contact_forms`
+--
+ALTER TABLE `contact_forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `deposits`
+--
+ALTER TABLE `deposits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `contact_forms`
+--
+ALTER TABLE `contact_forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+
+--
+-- AUTO_INCREMENT pour la table `deposits`
+--
+ALTER TABLE `deposits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
