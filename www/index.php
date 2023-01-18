921c5f5fcda3e38a70688fede0db7254d20c0aac
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-18 09:47:51 +0100

    save

--- www/index.php
+++ www/index.php
@@ -20,14 +20,10 @@ $page_title = 'Home page';
     </head>
     
     <body>
-        <header>
-            <?php require_once __DIR__ . '/../src/templates/partials/html_head.php'; ?>
-        </header>
+        <?php require_once __DIR__ . '/../src/templates/partials/html_head.php'; ?>
     
 
 
-        <footer>
         <?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
-    </footer>
     </body>
 </html>
