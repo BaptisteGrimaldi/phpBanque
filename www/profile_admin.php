76dfaa94057dff70800dab069a8f7486ad76b4c4
ScorpioOffi <leovejux95@gmail.com>
2023-01-19 18:25:40 +0100

    save avancement

--- www/profile_admin.php
+++ www/profile_admin.php
@@ -14,36 +14,67 @@ require_once __DIR__ . '/../src/init.php';
         <link rel="stylesheet" href="assets/css/profile_admin.css">
         <title>Banque Centrale</title>
         <link rel="shortcut icon" type="x-icon" href="assets/images/icon.png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     
     <body>
-        <?php require_once __DIR__ . '/../src/templates/partials/html_header.php'; ?>
+        <!--<?php require_once __DIR__ . '/../src/templates/partials/html_header.php'; ?>-->
     
-        <div class="side-menu">
-            <div class="brand-name">
-                <h2>Administration</h2>
-            </div>
-            <ul>
-                <li>Dépots</li>
-                <li>Contact</li>
-                <li>Administrateurs</li>
-            </ul>
-        </div>
         <div class="container">
-            <div class="header">
-                <div class="nav">
+            <div class="sidebar">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-user-secret"></i>
+                            <div class="title">Administration</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-piggy-bank"></i>
+                            <div class="title">Dépots</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-address-book"></i>
+                            <div class="title">Contact</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php">
+                        <i class="fa-solid fa-backward-step"></i>
+                            <div class="title">Retour au site</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="main">
+                <div class="top-bar">
                     <div class="search">
-                        <input type="text" placeholder="Recherche...">
-                        <button type="submit"><img src="assets/images/search.png" alt=""></button>
+                        <input type="text" name="search" placeholder="Rechercher...">
+                        <label for="search"><i class="fas fa-search"></i></label>
+                    </div>
+                    <i class="fas fa-bell"></i>
+                    <div class="user">
+                        <img src="assets/images/icon_user_default.png" alt="">
                     </div>
                 </div>
-                <div class="user">
-                    <a href="#"btn>Add New</a>
+                <div class="cards">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="num">67</div>
+                            <div class="card-name">Appointements</div>
+                        </div>
+                        <div class="icon-box">
+                            <i class="fas fa-briedcase-medical"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <?php require_once __DIR__ . '/../src/templates/partials/html_footer.php'; ?>
+        <!--<?php require_once __DIR__ . '/../src/templates/partials/html_footer.php'; ?>-->
     </body>
 </html>
